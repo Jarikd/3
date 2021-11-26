d1b67584584ec589cb30494a218f5ff617adad04
Jarikd <jarikd@ukr.net>
2021-11-26 14:43:17 +0200

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,6 @@
+  
+document.addEventListener("DOMContentLoaded", () => {
+    const CURRENT_YEAR = new Date().getFullYear();
+    const YEAR_ELEMENT = document.querySelector('.footer__year');
+    YEAR_ELEMENT.textContent = CURRENT_YEAR.getFullYear();
+  });
